320827911e1c9f444f098bf32e4c897110f3b01c
Satnam Singh <satnambajwa85@gmail.com>
2016-06-10 12:41:46 +0530

    json enlarged

--- app/templates/templateAll/template1/template1.component.js
+++ app/templates/templateAll/template1/template1.component.js
@@ -197,7 +197,185 @@ var Template1Component = (function () {
                                 ]
                             },
                         ]
-                    }]
+                    },
+                    {
+                        "sections": [{
+                                "order": 1,
+                                "items": [
+                                    {
+                                        "order": 1,
+                                        "type": "textfield",
+                                        "props": {
+                                            "title": "this checkbox section question",
+                                            "helpText": "SOME HELPER TEXT"
+                                        },
+                                        "config": {
+                                            "type": "text",
+                                            "attr": [{
+                                                    "class": "customeClass",
+                                                    "style": "heightcolorred"
+                                                }],
+                                            "validations": [{
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error"
+                                                }, {
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error",
+                                                    "min": "0",
+                                                    "max": "100"
+                                                }],
+                                            "maxSelections": 1,
+                                            "direction": "horizontal",
+                                            "placeholder": "this textfield in placeholder",
+                                            "defaultvalue": "Default Value"
+                                        },
+                                        "options": [{
+                                                "type": "toggel",
+                                                "lable": "OPTION 1",
+                                                "value": "10",
+                                                "selected": false,
+                                                "icon": "http://test.com/jd.png",
+                                                "attr": {
+                                                    "class": "customeClass",
+                                                    "style": "height:100;color:red;"
+                                                }
+                                            }]
+                                    },
+                                    {
+                                        "order": 2,
+                                        "type": "text-area",
+                                        "props": {
+                                            "title": "this checkbox section question",
+                                            "helpText": "SOME HELPER TEXT"
+                                        },
+                                        "config": {
+                                            "type": "text",
+                                            "attr": [{
+                                                    "class": "customeClass",
+                                                    "style": "heightcolorred"
+                                                }],
+                                            "validations": [{
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error"
+                                                }, {
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error",
+                                                    "min": "0",
+                                                    "max": "100"
+                                                }],
+                                            "maxSelections": 1,
+                                            "direction": "horizontal",
+                                            "placeholder": "this text area in placeholder",
+                                            "defaultvalue": "Default Value"
+                                        },
+                                        "options": [{
+                                                "type": "toggel",
+                                                "lable": "OPTION 1",
+                                                "value": "10",
+                                                "selected": false,
+                                                "icon": "http://test.com/jd.png",
+                                                "attr": {
+                                                    "class": "customeClass",
+                                                    "style": "height:100;color:red;"
+                                                }
+                                            }]
+                                    }
+                                ]
+                            },
+                            {
+                                "order": 2,
+                                "items": [
+                                    {
+                                        "order": 1,
+                                        "type": "text-area",
+                                        "props": {
+                                            "title": "this checkbox section question",
+                                            "helpText": "SOME HELPER TEXT"
+                                        },
+                                        "config": {
+                                            "type": "text",
+                                            "attr": [{
+                                                    "class": "customeClass",
+                                                    "style": "heightcolorred"
+                                                }],
+                                            "validations": [{
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error"
+                                                }, {
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error",
+                                                    "min": "0",
+                                                    "max": "100"
+                                                }],
+                                            "maxSelections": 1,
+                                            "direction": "horizontal",
+                                            "placeholder": "this text areea in placeholder",
+                                            "defaultvalue": "Default Value"
+                                        },
+                                        "options": [{
+                                                "type": "toggel",
+                                                "lable": "OPTION 1",
+                                                "value": "10",
+                                                "selected": false,
+                                                "icon": "http://test.com/jd.png",
+                                                "attr": {
+                                                    "class": "customeClass",
+                                                    "style": "height:100;color:red;"
+                                                }
+                                            }]
+                                    },
+                                    {
+                                        "order": 2,
+                                        "type": "textfield",
+                                        "props": {
+                                            "title": "this checkbox section question",
+                                            "helpText": "SOME HELPER TEXT"
+                                        },
+                                        "config": {
+                                            "type": "text",
+                                            "attr": [{
+                                                    "class": "customeClass",
+                                                    "style": "heightcolorred"
+                                                }],
+                                            "validations": [{
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error"
+                                                }, {
+                                                    "required": "true",
+                                                    "message": "This field is required",
+                                                    "errorClass": "error",
+                                                    "min": "0",
+                                                    "max": "100"
+                                                }],
+                                            "maxSelections": 1,
+                                            "direction": "horizontal",
+                                            "placeholder": "this textfield in placeholder",
+                                            "defaultvalue": "Default Value"
+                                        },
+                                        "options": [{
+                                                "type": "toggel",
+                                                "lable": "OPTION 1",
+                                                "value": "10",
+                                                "selected": false,
+                                                "icon": "http://test.com/jd.png",
+                                                "attr": {
+                                                    "class": "customeClass",
+                                                    "style": "height:100;color:red;"
+                                                }
+                                            }]
+                                    }
+                                ]
+                            },
+                        ]
+                    }
+                ]
             }
         };
     }
