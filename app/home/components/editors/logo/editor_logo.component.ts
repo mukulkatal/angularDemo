c1d97b83c864adbd69240330fc27d6119cc96c97
Naman Kareer <naman.kareer@venturepact.com>
2016-06-27 16:05:05 +0530

    changes

--- app/home/components/editors/logo/editor_logo.component.ts
+++ app/home/components/editors/logo/editor_logo.component.ts
@@ -2,18 +2,36 @@ import { Component, Input } from '@angular/core';
 @Component({
 	selector: 'editor-logo',
 	template:`
-		<div>
-			URL :<input type="text" [(ngModel)] = "control.props.title" />
-			Help Text : <input type="text" [(ngModel)] = "control.props.helpText" />
-			
-			Alter Text : <input type="text" [(ngModel)] = "control.config.placeholder" />
-			Width:<input type="text" [(ngModel)] = "control.config.attr.width" />
-			Height:<input type="text" [(ngModel)] = "control.config.attr.height" />
-		</div>		
+		 <div class="logo-details">
+			<span>{{control.type}}:</span>
+			<div class="desc-switch">
+				<input type="checkbox" name="properties-checkbox" checked data-size="mini" [(ngModel)] = "control.config.showControl">
+			</div>
+			<div class="landing-inputfile">
+				<div class="col-xs-12 no-padding">
+					<div class="input-group">
+						<input type="text" class="file-input" placeholder="No file" readonly [(ngModel)] = "control.props.title">
+						<label class="input-group-btn">
+							<span class="btn btn-default btn-flat btn-basic2">
+							Upload <input type="file" style="display: none;">
+						</span>
+						</label>
+					</div>
+				</div>
+			</div>
+		</div>
 	`
 
-})//<input type="text" [(ngModel)] = "control.config.placeholder" />
+})
+/*<input type="text" [(ngModel)] = "control.config.placeholder" />
+<div>
+	URL :<input type="text" [(ngModel)] = "control.props.title" />
+	Help Text : <input type="text" [(ngModel)] = "control.props.helpText" />
 	
+	Alter Text : <input type="text" [(ngModel)] = "control.config.placeholder" />
+	Width:<input type="text" [(ngModel)] = "control.config.attr.width" />
+	Height:<input type="text" [(ngModel)] = "control.config.attr.height" />
+</div>*/
 
 export class EditorLogo
 {
