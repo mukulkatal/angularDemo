b5ec79af023b26f3c69dba6b4472b0639069fb7e
Satnam Singh <satnambajwa85@gmail.com>
2016-06-27 13:37:35 +0530

    chk

--- app/home/components/editor.js
+++ app/home/components/editor.js
@@ -1,10 +1,10 @@
 "use strict";
-var editor_selectbox_component_1 = require('./editors/editor_selectbox.component');
-var editor_textfield_component_1 = require('./editors/editor_textfield.component');
-var editor_header_component_1 = require('./editors/editor_header.component');
-var editor_button_component_1 = require('./editors/editor_button.component');
-var editor_logo_component_1 = require('./editors/editor_logo.component');
-var editor_slider_component_1 = require('./editors/editor_slider.component');
+var editor_selectbox_component_1 = require('./editors/selectbox/editor_selectbox.component');
+var editor_textfield_component_1 = require('./editors/textfield/editor_textfield.component');
+var editor_header_component_1 = require('./editors/header/editor_header.component');
+var editor_button_component_1 = require('./editors/button/editor_button.component');
+var editor_logo_component_1 = require('./editors/logo/editor_logo.component');
+var editor_slider_component_1 = require('./editors/slider/editor_slider.component');
 exports.EDITORS = [
     editor_selectbox_component_1.EditorSelectBox,
     editor_textfield_component_1.EditorTextField,
