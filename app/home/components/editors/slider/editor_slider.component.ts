477ae07e0d9ae28947f20986d1909499755fd078
Abhishek Singh <Abhishek Singh>
2016-06-28 13:04:15 +0530

    header and slider editor

--- app/home/components/editors/slider/editor_slider.component.ts
+++ app/home/components/editors/slider/editor_slider.component.ts
@@ -4,10 +4,37 @@ import { JSONElement } from '../../../services/JSONElement.service';
 @Component({
 	selector:'editor-slider',
 	template:`
-		Help Text : <input type="text" [(ngModel)] = "control.props.helpText" />
-		Title :<input type="text" [(ngModel)] = "control.props.title" />
-		PlaceHolder : <input type="text" [(ngModel)] = "control.config.placeholder" />
+		<div class="type-details">
+                    <div class="col-xs-12 no-padding text-group">
+                        <span class="col-md-4 no-padding form-label">Title :</span>
+                        <div class="col-md-8">
+                            <input type="text" class="form-text" [(ngModel)] = "control.props.title" >
+                        </div>
+                    </div>
+                    <div class="col-xs-12 no-padding text-group ">
+                        <span class=" col-md-4 no-padding form-label">	Help Text :</span>
+                        <div class="col-md-8">
+                            <input type="text" class="form-text"  [(ngModel)] = "control.props.helpText">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 no-padding text-group ">
+                        <span class="col-md-4 no-padding form-label">PlaceHolder :</span>
+                        <div class="col-md-8">
+                            <input type="text" class="form-text" [(ngModel)]="control.config.placeholder">
+                        </div>
+                    </div>
+                </div>
+                <div class="type-details">
+                            <label class="check">
+                                <input type="checkbox" value="Value1 " name="checkbox1" id="check1">
+                                <label for="check1"> </label>
+                                Mark as Mandatory
+                            </label>
+                </div>
 	`
+	// Help Text : <input type="text" [(ngModel)] = "control.props.helpText" />
+	// 	Title :<input type="text" [(ngModel)] = "control.props.title" />
+	// 	PlaceHolder : <input type="text" [(ngModel)] = "control.config.placeholder" />
 })
 
 export class EditorSlider
